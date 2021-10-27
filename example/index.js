9869ae3a7a7ef2a74fe1e93f897059162c46bdb3
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2021-10-27 16:57:48 -0700

    Hook up dispatcher, timer, and reload

--- example/index.js
+++ example/index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+// We need to import something from react-native, so that react-native is included in the bundle correctly.
+import {NativeModules} from 'react-native'; 
+
 const J = 'Java';
 const S = 'Script';
 nativeConsole.log(`Hello from ${J}${S}!`);
